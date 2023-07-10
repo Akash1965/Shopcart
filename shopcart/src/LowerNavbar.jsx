374bad30b5f8eeff541b3190f6f9fa99e683e681
Akash Gupta <akashg.official18@gmail.com>
2023-07-11 03:58:28 +0530

    UpperNavbar added and LowerNavbar modification required

---
+++ shopcart/src/LowerNavbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './lowernavbar.css'
+
+export const LowerNavbar = () => {
+  return (
+    <nav className='d-flex flex-md-row justify-content-md-around flex-column text-center bg-success text-white'>
+            <li><i className="bi bi-telephone"></i>+001234567890</li>
+            <li>Get 50% Off on Selected Items    |      Shop Now</li>
+            <div>
+                <select name="" id="" className='bg-success text-white'>
+                    <option value="" selected disabled hidden>Eng</option>
+                    <option value="">Bangla</option>
+                    <option value="">Arabic</option>
+                    <option value="">Urdu</option>
+                </select>
+                <select name="" id="" className='bg-success text-white'>
+                    <option value="" selected disabled hidden>Location</option>
+                    <option value="">Dhaka</option>
+                    <option value="">USA</option>
+                    <option value="">India</option>
+                </select>
+            </div>
+    </nav>
+  )
+}
---
+++ shopcart/src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { LowerNavbar } from './LowerNavbar'
+import { UpperNavbar } from './UpperNavbar'
+
+export const Navbar = () => {
+  return (
+    <>
+        <LowerNavbar/>
+        <UpperNavbar/>
+    </>
+  )
+}
